807039449a79eb2eaeb7b444156653a7d2fddddd
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-12-04 11:24:22 -0500

    clear project, add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'Shoshana';
-}
+export class AppComponent {}
