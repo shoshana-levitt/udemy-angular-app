79b2c75da172b56a659f024e9787dd5e5dcdd3e0
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-12-12 15:29:31 -0500

    add data binding to ServersComponent input

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
+  serverName = '';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,5 +23,8 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
