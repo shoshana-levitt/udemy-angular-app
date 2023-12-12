519374b062639d050bf7ba93d9ec2ebe2ceb0dd1
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-12-12 15:46:31 -0500

    change serverName upon clicking button on ServersComponent

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -22,7 +22,7 @@ export class ServersComponent implements OnInit {
   }
   ngOnInit() {}
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
   }
   onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
