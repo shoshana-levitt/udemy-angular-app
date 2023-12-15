a42d8f371ae44f2f2568baa2651e1e05e51f52f1
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-12-15 11:12:07 -0500

    add ngIf directive to ServersComponent

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Test Server';
+  serverCreated = false;
   username = '';
   constructor() {
     setTimeout(() => {
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
   onCreateServer() {
     this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
+    this.serverCreated = true;
   }
   onUpdateServerName(event: any) {
     this.serverName = (<HTMLInputElement>event.target).value;
